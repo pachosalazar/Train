4917155c8b155a1c2d7201284e7b82611368c0c5
Pacho Salazar <jfsh87@hotmail.com>
2018-03-30 19:37:45 -0400

    starting

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAA5yx8HSylFw700ow2vGjil_S07vioO2Y",
+    authDomain: "train-36d36.firebaseapp.com",
+    databaseURL: "https://train-36d36.firebaseio.com",
+    projectId: "train-36d36",
+    storageBucket: "",
+    messagingSenderId: "492987779586"
+  };
+  firebase.initializeApp(config);
